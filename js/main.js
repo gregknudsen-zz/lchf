a1b4d3dff32dcfcc86d559ba34e5cdd69b6b7bee
Gregory Knudsen <dev.artist15@gmail.com>
2016-07-21 17:13:21 -0400

    add more hover animate effects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-$("p").hover(function () {
-	$(this).removeClass("rotateIn").addClass("shake");
-}, function(){$(this).removeClass("shake");
+$(function(){
+	$("p").hover(function () {
+		$(this).removeClass("rotateIn").addClass("shake");
+	}, 
+		function(){$(this).removeClass("shake");
+	});
+
+	$(".logo").hover(function () {
+		$(this).removeClass("fadeInUpBig").addClass("shake");
+	}, 
+		function(){$(this).removeClass("shake");
+	});
+
+	$(".smiley").hover(function () {
+		$(this).removeClass("fadeInRightBig").addClass("pulse");
+	}, 
+		function(){$(this).removeClass("pulse");
+	});
 });
