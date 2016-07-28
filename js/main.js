8ec2251fb58a562cd977734684ca7bad12b57162
Gregory Knudsen <dev.artist15@gmail.com>
2016-07-28 12:23:10 -0400

    add more minor jQuery effects and link to youtube page

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@ $(function(){
 	$("p").hover(function () {
 		$(this).removeClass("rotateIn").addClass("shake");
 	}, 
-		function(){$(this).removeClass("shake");
+		function() {$(this).removeClass("shake");
 	});
 
 	$(".logo").hover(function () {
 		$(this).removeClass("fadeInUpBig").addClass("shake");
 	}, 
-		function(){$(this).removeClass("shake");
+		function() {$(this).removeClass("shake");
 	});
 
 	$(".smiley").hover(function () {
 		$(this).removeClass("fadeInRightBig").addClass("pulse");
 	}, 
-		function(){$(this).removeClass("pulse");
+		function() {$(this).removeClass("pulse");
 	});
 });
